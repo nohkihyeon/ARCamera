d92a5bd75953dd56b9dfecc65fee6a331fc9c730
nohkihyeon <65120581+nohkihyeon@users.noreply.github.com>
2021-08-11 01:12:08 +0900

    revert
    revert can move

---
+++ Assets/Scripts/ObjectTouchEvent.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectTouchEvent : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        ClickDetect();
+    }
+
+    private void ClickDetect()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Debug.Log("GetMouseButton0");
+            RaycastHit hit_info;
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            if (Physics.Raycast(ray, out hit_info, Mathf.Infinity))
+            {
+                Debug.Log("충돌체 이름 : " + hit_info.collider.gameObject.name);
+                Debug.Log("충돌거리 : " + hit_info.distance);
+                Debug.Log("법선벡터 : " + hit_info.normal);
+            }
+        }
+    }
+}
