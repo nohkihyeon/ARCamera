cc18b58d5882335d4b15065a4ad05fe7720afc03
nohkihyeon <65120581+nohkihyeon@users.noreply.github.com>
2021-08-26 20:53:45 +0900

    feat : take a photo
    UnauthorizedAccessException 해결했지만 사진 문서가 저장되는 경로를 확인하지 못함

--- Assets/Scripts/CameraCapture.cs
+++ Assets/Scripts/CameraCapture.cs
@@ -1,8 +1,11 @@
 ﻿using System.IO;
 using UnityEngine;
+using UnityEngine.UI;
  
 public class CameraCapture : MonoBehaviour
 {
+    [SerializeField]
+    private Button TakePhotoButton;
     public int fileCounter;
     public KeyCode screenshotKey;
     private Camera Camera
@@ -18,14 +21,23 @@ public class CameraCapture : MonoBehaviour
         }
     }
     private Camera _camera;
- 
-    private void LateUpdate()
+    void Awake()
     {
-        if (Input.GetKeyDown(screenshotKey))
-        {
-            Capture();
-        }
+        TakePhotoButton.onClick.AddListener(TakePhoto);
     }
+
+    private void TakePhoto()
+    {
+        Debug.Log("TakePhoto()" + fileCounter);
+        Capture();
+    }
+    // private void LateUpdate()
+    // {
+    //     if (Input.GetKeyDown(screenshotKey))
+    //     {
+    //         Capture();
+    //     }
+    // }
  
     public void Capture()
     {
@@ -41,8 +53,14 @@ public class CameraCapture : MonoBehaviour
  
         byte[] bytes = image.EncodeToPNG();
         Destroy(image);
- 
-        File.WriteAllBytes(Application.dataPath + "/Image/" + fileCounter + ".png", bytes);
+
+        // Mac path : 
+        // cf) https://docs.unity3d.com/ScriptReference/Application-dataPath.html?_ga=2.85471643.307249663.1629898360-1836162968.1629898360&_gl=1*1kwusal*_ga*MTgzNjE2Mjk2OC4xNjI5ODk4MzYw*_ga_1S78EFL1W5*MTYyOTkwMDcyMi4yLjAuMTYyOTkwMDcyMi42MA..
+        // File.WriteAllBytes(Application.dataPath + "/Image/" + fileCounter + ".png", bytes);
+
+        // cf2) https://devparklibrary.tistory.com/32
+        Debug.Log(Application.persistentDataPath);
+        File.WriteAllBytes(Application.persistentDataPath +"/Image" +fileCounter + ".png", bytes);
         fileCounter++;
     }
 }
