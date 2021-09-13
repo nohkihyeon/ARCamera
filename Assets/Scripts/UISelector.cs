b4123cdaa87a45fac94b75ba3e7be56bf7322a50
nohkihyeon <65120581+nohkihyeon@users.noreply.github.com>
2021-09-14 01:48:00 +0900

    feat : Object can adjust size
    feat : Object can adjust size by two touch

--- Assets/Scripts/UISelector.cs
+++ Assets/Scripts/UISelector.cs
@@ -68,9 +68,13 @@ public class UISelector : MonoBehaviour
     public Transform[] SelectPoseInfor = new Transform[k_NumSkeletonJoints];
     Transform[] PoseTransform;
 
-    //public Text TestText1;
     int number;
     bool PoseSelectState;
+
+    // for object two touch
+    private float initialDistance;
+    private Vector3 initialScale;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -117,6 +121,7 @@ public class UISelector : MonoBehaviour
     {
         TestText2.text = StateTest.ToString();
         TouchControll();
+        // twoTouchEvent();
         if (!PoseSelectState)
         {
             isSelected = true;
@@ -280,6 +285,48 @@ public class UISelector : MonoBehaviour
             startPos = touch.position;
         }
 
+    }
+    private void twoTouchEvent()
+    {
+        if (Input.touchCount == 2)
+        {
+            Touch touchZero = Input.GetTouch(0); 
+            Touch touchOne = Input.GetTouch(1);
+
+            // if one of the touches Ended or Canceled do nothing
+            if(touchZero.phase == TouchPhase.Ended || touchZero.phase == TouchPhase.Canceled  
+            || touchOne.phase == TouchPhase.Ended || touchOne.phase == TouchPhase.Canceled) 
+            {
+                return;
+            }
+
+            // It is enough to check whether one of them began since we
+            // already excluded the Ended and Canceled phase in the line before
+            if(touchZero.phase == TouchPhase.Began || touchOne.phase == TouchPhase.Began)
+            {
+                // track the initial values
+                initialDistance = Vector2.Distance(touchZero.position, touchOne.position);
+                initialScale = SelectedPose.transform.localScale;
+            }
+            // else now is any other case where touchZero and/or touchOne are in one of the states
+            // of Stationary or Moved
+            else
+            {
+                // otherwise get the current distance
+                var currentDistance = Vector2.Distance(touchZero.position, touchOne.position);
+
+                // A little emergency brake ;)
+                // if(Mathf.Approximately(initialDistance, 0)) return;
+
+                // get the scale factor of the current distance relative to the inital one
+                var factor = currentDistance / initialDistance;
+
+                // apply the scale
+                // instead of a continuous addition rather always base the 
+                // calculation on the initial and current value only
+                SelectedPose.transform.localScale = initialScale * factor;
+            }
+        }
     }
     private void ResetPoseScaler()
     {
